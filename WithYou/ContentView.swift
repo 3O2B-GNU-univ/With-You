59902aef5dba48ab6f7a88f6eca5cf783037fa01
junoyou <ryuwon_@naver.com>
2023-05-21 21:18:47 +0900

    FEAT: 설정, 초기화면 업데이트

---
+++ WithYou/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  WithYou
+//
+//  Created by 최성빈 on 2023/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 1
+    
+    
+    var body: some View {
+        NavigationView {
+            TabView(selection: $selection) {
+                Text("Tab Content 1").tabItem { Image(systemName: "magnifyingglass") }.tag(1)
+                WithYouView().tabItem { Image(systemName: "person.2") }.tag(2)
+                Text("Tab Content 1").tabItem { Image(systemName: "message") }.tag(3)
+                SettingsView().tabItem { Image(systemName: "gearshape") }.tag(4)
+            }
+            .accentColor(Color("mainColor"))
+            .onAppear {
+                UITabBar.appearance().backgroundColor = .white
+            }
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ WithYou/Model/UserProfile.swift
@@ -0,0 +1,29 @@
+//
+//  UserProfile.swift
+//  WithYou
+//
+//  Created by 최성빈 on 2023/05/01.
+//
+
+import Foundation
+
+class User: Identifiable, Codable {
+    var id: UUID
+    var name: String
+    var imgStr: String
+
+    init(id: UUID = UUID(), name: String, imgStr: String = "default") {
+        self.id = id
+        self.name = name
+        self.imgStr = imgStr
+    }
+
+}
+
+
+
+
+
+
+
+
