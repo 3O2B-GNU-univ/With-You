516bb6c0c21f3fb77364bdf23d40c3bc41c2ffe7
junoyou <ryuwon_@naver.com>
2023-06-01 00:55:47 +0900

    FEAT: pull 및 카카오톡 로그인 추가

---
+++ WithYou/WithYouApp.swift
@@ -0,0 +1,26 @@
+//
+//  WithYouApp.swift
+//  WithYou
+//
+//  Created by 최성빈 on 2023/04/02.
+//
+
+import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
+
+@main
+struct WithYouApp: App {
+
+    init() {
+        //kakao 네이티브 앱 키 세팅
+        let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+        KakaoSDK.initSDK(appKey:"\(kakaoAppKey)")
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
